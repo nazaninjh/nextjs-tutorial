55e6c7b4d2873009cc0a0446fac626e0ed53b611
nazaninjh <nazanin.jh97@gmail.com>
2024-05-31 17:14:16 +0330

    corrected

---
+++ app/layout.jsx
@@ -0,0 +1,9 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
+export default function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
